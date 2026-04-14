87dc972267a587e5ee2b51c3649e2c8c75e5a4e0
tigerflare25-coder <tigerflare25@gmail.com>
2026-04-14 21:01:28 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,15 +5,30 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+from django.utils import timezone # Add this for time calculations
+
 class User(AbstractUser):
-    # Make email unique so Allauth can use it as the primary ID
     email = models.EmailField(unique=True)
     is_premium = models.BooleanField(default=False)
+    # New Field: Track when the subscription ends
+    premium_expiry = models.DateTimeField(null=True, blank=True) 
     logo = CloudinaryField('image', null=True, blank=True)
 
-    # USERNAME_FIELD is 'username' by default. 
-    # REQUIRED_FIELDS must NOT include 'username' for auto-signup to work smoothly.
     REQUIRED_FIELDS = [] 
+
+    def check_premium_status(self):
+        """
+        Call this in your views/templates to see if user is STILL pro.
+        If the current time is past expiry, it flips them to False.
+        """
+        if self.is_premium and self.premium_expiry:
+            if timezone.now() > self.premium_expiry:
+                self.is_premium = False
+                self.save()
+        return self.is_premium
 
     def __str__(self):
         return self.email
